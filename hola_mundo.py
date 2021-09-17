2112cab6ce64e1fd86ff635f3bd7a87259cdf4b8
Francisco Mendez <pacomendez12@gmail.com>
2021-09-16 20:13:53 -0500

    Mi primer programa

---
+++ hola_mundo.py
@@ -0,0 +1,31 @@
+message1 = 'Hola mundo 😀'
+message2 = "Adios mundo"
+
+print(message1)
+print(message2)
+
+
+print(message2.upper())
+print(message2.lower())
+
+
+nombre = "Paco"
+apellido = "Mendez"
+
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+
+nombre_completo_f_string = f"{nombre} {apellido}"
+print(nombre_completo_f_string)
+
+
+
+texto = "uno\ndos\ttres"
+print(texto)
+
+
+mi_mensaje = "        Hola       "
+print(mi_mensaje.strip())
+
+print(f"{nombre} {apellido}")
